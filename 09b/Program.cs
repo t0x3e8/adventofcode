745ade839c8461669a4f3b8047b068e59353d4c5
t0x3e8 <t0x3e8@gmail.com>
2019-02-01 13:28:57 +0100

    Moved to LinkedList

--- 09b/Program.cs
+++ 09b/Program.cs
@@ -11,10 +11,13 @@ namespace _09b
 {
     class Program
     {
-        
+
         static readonly int numberOfPlayers = 476;
+        // for lastMarble = 116570, for numberOfPlayers = 476
+        // #121 with the score of 953674.
+        // Stopwatch stops: 11.173167
+
         static readonly int lastMarble = 7165700;
-        static readonly bool canPrint2Console = false;
 
         static void Main(string[] args)
         {
@@ -22,74 +25,65 @@ namespace _09b
             sw.Start();
             Console.WriteLine($"StopWatch started.");
 
-            Dictionary<int, int> players = InitializePlayers(numberOfPlayers);
-            players = Play(players, lastMarble);       
+            Dictionary<int, long> players = InitializePlayers(numberOfPlayers);
+            players = Play2(players, lastMarble);
 
             var winningPlayer = players.OrderByDescending(kv => kv.Value).First();
 
-            Console.WriteLine($"The winning Elf is #{winningPlayer.Key} with the score of {winningPlayer.Value}.");
+            Console.WriteLine($"The winning Elf is #{winningPlayer.Key} with the score of: {winningPlayer.Value}.");
 
             sw.Stop();
             Console.WriteLine($"Stopwatch stops: {sw.Elapsed.TotalSeconds}");
         }
 
-        private static Dictionary<int, int> InitializePlayers(int numberOfPlayers)
+        private static Dictionary<int, long> InitializePlayers(int numberOfPlayers)
         {
-            Dictionary<int, int> players = new Dictionary<int, int>();
-            for (int player = 1; player <= numberOfPlayers; player++) {
+            Dictionary<int, long> players = new Dictionary<int, long>();
+            for (int player = 1; player <= numberOfPlayers; player++)
+            {
                 players.Add(player, 0);
             }
 
             return players;
         }
 
-        private static Dictionary<int, int> Play(Dictionary<int, int> players, int lastMarble)
+        private static Dictionary<int, long> Play2(Dictionary<int, long> players, int lastMarble)
         {
-            int currentMarble = 0;
-            int nextMarble = 0;
-            List<int> circle = new List<int>();
-            circle.Add(currentMarble);
+            int currentMarbleValue = 0;
+            LinkedList<int> circle = new LinkedList<int>();
+            LinkedListNode<int> currentMarbleNode = circle.AddFirst(currentMarbleValue);
+            Dictionary<int, int> playersScoresInTurn = new Dictionary<int, int>();
 
-            if (canPrint2Console)
-                Print(circle, 0, 0);
-            
-            while(currentMarble < lastMarble) {
-                Dictionary<int, int> playersScoresInTurn = new Dictionary<int, int>();
-                foreach(var player in players){
-                    nextMarble++;
+            while (currentMarbleValue < lastMarble)
+            {
+                playersScoresInTurn.Clear();
+                foreach (var player in players)
+                {
+                    currentMarbleValue++;
+                    //Console.WriteLine($" {string.Join(' ', circle.ToArray())}, Cur: {currentMarbleNode.Value}");
 
-                    // add values to temp collection, as the players dict. cannot be modified
-                    if ((nextMarble % 23) == 0) {
-
-                        int indexOfCurentMarble = circle.IndexOf(currentMarble);
-                        // if the index of current marble is less then the value of shift
-                        if (indexOfCurentMarble < 7)
-                            indexOfCurentMarble = circle.Count - Math.Abs(indexOfCurentMarble - 7);
-                        else 
-                            indexOfCurentMarble = indexOfCurentMarble - 7;
-                        
-                        playersScoresInTurn.Add(player.Key, nextMarble + circle.ElementAt(indexOfCurentMarble));
-
-                        circle.RemoveAt(indexOfCurentMarble);
-                        currentMarble = circle.ElementAt(indexOfCurentMarble);
-                        
-                        if (canPrint2Console) Print(circle, currentMarble, player.Key);
+                    if (currentMarbleValue > 0 && (currentMarbleValue % 23) == 0)
+                    {
+                        currentMarbleNode = FindPrevSeventhNode(currentMarbleNode);
+                        int removedValue = RemovePrevNode(currentMarbleNode);
+                        playersScoresInTurn.Add(player.Key, removedValue + currentMarbleValue);
                     }
-                    else {    
-                        int marbleInsertIndex = DetermineInsertionIndex(circle, currentMarble);
-                        
-                        circle.Insert(marbleInsertIndex, nextMarble);   
-                        
-                        if (canPrint2Console) Print(circle, nextMarble, player.Key);
-                        
-                        currentMarble = nextMarble;
+                    else
+                    {
+                        if (currentMarbleNode.Next == null && currentMarbleNode.Previous == null)
+                            currentMarbleNode = circle.AddAfter(currentMarbleNode, currentMarbleValue);
+                        else if (currentMarbleNode.Next != null)
+                            currentMarbleNode = circle.AddAfter(currentMarbleNode.Next, currentMarbleValue);
+                        else
+                            currentMarbleNode = circle.AddAfter(circle.First, currentMarbleValue);
                     }
 
-                    if (nextMarble == lastMarble)
+                    if (currentMarbleValue == lastMarble)
                         break;
                 }
 
-                foreach(var playerScore in playersScoresInTurn) {
+                foreach (var playerScore in playersScoresInTurn)
+                {
                     players[playerScore.Key] += playerScore.Value;
                 }
             }
@@ -97,34 +91,33 @@ namespace _09b
             return players;
         }
 
-        private static int DetermineInsertionIndex(List<int> circle, int currentMarble)
-        {  
-            int currentIndex = circle.FindIndex(0, circle.Count, i => i == currentMarble);
-
-            int result = (currentIndex + 1) >= circle.Count ? 1 : currentIndex + 2;
+        private static int RemovePrevNode(LinkedListNode<int> currentMarbleNode)
+        {            
+            int result = 0;
+            var list = currentMarbleNode.List;
+            if (currentMarbleNode.Previous == null) {
+                result = list.Last.Value;
+                list.Remove(result);
+            }
+            else {
+                result = currentMarbleNode.Previous.Value;
+                list.Remove(result);
+            }
 
             return result;
         }
 
-        private static void Print(List<int> circle, int currentMarble, int playerId)
+        private static LinkedListNode<int> FindPrevSeventhNode(LinkedListNode<int> currentMarbleNode)
         {
-            int currentMarbleIndex = circle.IndexOf(currentMarble);
+            for (int i = 0; i < 6; i++)
+            {
+                if (currentMarbleNode.Previous != null)
+                    currentMarbleNode = currentMarbleNode.Previous;
+                else
+                    currentMarbleNode = currentMarbleNode.List.Last;
+            }
 
-            Console.Write($"[{playerId}]   ");
-
-            // print before current marble
-            if (currentMarbleIndex > 0)
-                Console.Write($" {string.Join(' ', circle.GetRange(0, currentMarbleIndex))} ");
-            
-            // print current marble
-            Console.Write($"({circle[currentMarbleIndex]})");
-
-            // print after current marble
-            // if( currentMarbleIndex + 1 < circle.Count)
-                Console.Write($" {string.Join(' ', circle.GetRange(currentMarbleIndex + 1, circle.Count - currentMarbleIndex - 1))} ");
-            
-
-            Console.WriteLine();
+            return currentMarbleNode;
         }
     }
 }
